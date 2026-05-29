d214cded7a8bf2916a40f649b8995c0b242395cc
Nisha-pr <nisha45688@gmail.com>
2026-05-29 20:58:02 +0530

    Fix eslint warnings for production build

--- client/src/pages/admin/AdminDashboard.jsx
+++ client/src/pages/admin/AdminDashboard.jsx
@@ -20,6 +20,7 @@ const AdminDashboard = () => {
       return
     }
     fetchData()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [user, navigate])
 
   const getHeaders = () => ({
