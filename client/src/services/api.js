a96ba49f6124dbec3ee5c58bc9def7798b3f0a3f
Nisha-pr <nisha45688@gmail.com>
2026-05-31 21:34:40 +0530

    Fix API URL to production backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,10 +1,9 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://entreskill-hub-api.onrender.com/api'
 })
 
-// Har request mein token automatically add ho
 API.interceptors.request.use((req) => {
   const user = localStorage.getItem('userInfo')
   if (user) {
@@ -13,16 +12,8 @@ API.interceptors.request.use((req) => {
   return req
 })
 
-// Auth APIs
 export const registerAPI = (data) => API.post('/auth/register', data)
 export const loginAPI = (data) => API.post('/auth/login', data)
 export const getProfileAPI = () => API.get('/auth/profile')
-
-// Resource APIs
 export const getResourcesAPI = (params) => API.get('/resources', { params })
 export const createResourceAPI = (data) => API.post('/resources', data)
-
-// Progress APIs
-export const getUserProgressAPI = () => API.get('/progress')
-export const saveProgressAPI = (data) => API.post('/progress', data)
-export const deleteProgressAPI = (roadmapId) => API.delete(`/progress/${roadmapId}`)
